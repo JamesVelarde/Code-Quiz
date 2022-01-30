52d3d0a0b8540fba64268911277afc921e7e5b5d
James <jimmyvelarde3302@gmail.com>
2022-01-29 19:38:04 -0700

    Made questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,22 @@
 const intro = document.getElementById('intro')
 const startBtn = document.getElementById('start-btn')
+const submitBtn = document.getElementById('submit')
 const quiz = document.getElementById('quiz')
 
-const question = document.getElementById('question')
-const answer = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const answerEls = document.querySelectorAll('.answer')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
-
 startBtn.addEventListener("click", startQuiz);
-
 let currentQuiz = 0
 let score = 0
-
 // startQuiz()
 
 function startQuiz() {
-  const current
+  quiz.style.display = "block";
+  intro.style.display= "none";
   deselectAnswers()
   const currentQuizData = quizData[currentQuiz]
 
@@ -29,70 +26,79 @@ function startQuiz() {
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 }
-
 function deselectAnswers() {
     answerEls.forEach(answerEl => answerEl.checked = false)
 }
-
 function getSelected() {
     let answer
-
     answerEls.forEach(answerEl => {
         if(answerEl.checked) {
             answer = answerEl.id
         }
     })
-
     return answer
 }
 
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
 
-const quizContent = [
+  if(answer) {
+      if(answer === quizData[currentQuiz].correct) {
+          score++
+      }
 
+      currentQuiz++
+
+      if(currentQuiz < quizData.length) {
+          loadQuiz()
+      } else {
+          quiz.innerHTML = `
+              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+              <button onclick="location.reload()">Reload</button>
+          `
+      }
+  }
+})
 
 const quizData = [
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    a: "a",
-    b: "b",
-    c: "c",
-    d: "d",
-    correct: "a",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correct: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correct: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correct: "",
-  },
-  {
-    question: "?",
-    a: "",
-    b: "",
-    c: "",
-    d: "",
-    correct: "",
-  },
-];
+    {
+        question: "Commonly used data type do NOT include:",
+        a: "<strings>",
+        b: "<booleans>",
+        c: "<alerts>",
+        d: "<numbers>",
+        correct: "b",
+      },
+      {
+        question: "The condition in a if/else statement is enclosed with ______.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets",
+        correct: "c",
+      },
+      {
+        question: "Which built-in method sorts the elements of an array?",
+        a: "changeOrder(order)",
+        b: "order()",
+        c: "sort()",
+        d: "array_sorter",
+        correct: "c",
+      },
+      {
+        question: "Which of the following function of Array object reverses the order of the elements of an array?",
+        a: "reverse()",
+        b: "push()",
+        c: "reduce()",
+        d: "reuseRecycle()",
+        correct: "a",
+      },
+      {
+        question: "Which of the following is not a reserved word in JavaScript?",
+        a: "interface",
+        b: "throws",
+        c: "program",
+        d: "short",
+        correct: "c",
+      },
+    ];
